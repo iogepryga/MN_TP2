cc1f43ef9feb38faafcfd32688a411edda259539
Loris Mottino <loris.mottino@etu.univ-grenoble-alpes.fr>
2021-03-09 09:04:37 +0100

    Done complexe.c

--- TPBLAS/src/swap.c
+++ TPBLAS/src/swap.c
@@ -1,40 +1,36 @@
 #include "mnblas.h"
 
 void mncblas_sswap(const int N, float *X, const int incX, 
-                 float *Y, const int incY)
-{
-  register unsigned int i = 0 ;
-  register unsigned int j = 0 ;
-  register float save ;
-  
-  for (; ((i < N) && (j < N)) ; i += incX, j+=incY)
-    {
-      save = Y [j] ;
-      Y [j] = X [i] ;
-      X [i] = save ;
-    }
+								 float *Y, const int incY) {
+	register unsigned int i = 0;
+	register unsigned int j = 0;
+	register float save;
+	
+	for (; ((i < N) && (j < N)); i += incX, j+=incY)
+		{
+			save = Y [j];
+			Y [j] = X [i];
+			X [i] = save;
+		}
 
-  return ;
+	return;
 }
 
 void mncblas_dswap(const int N, double *X, const int incX, 
-                 double *Y, const int incY)
-{
+								 double *Y, const int incY) {
 
-  return ;
+	return;
 }
 
 void mncblas_cswap(const int N, void *X, const int incX, 
-		                    void *Y, const int incY)
-{
+												void *Y, const int incY) {
 
-  return ;
+	return;
 }
 
 void mncblas_zswap(const int N, void *X, const int incX, 
-		                    void *Y, const int incY)
-{
+												void *Y, const int incY) {
 
-  return ;
+	return;
 }
 
